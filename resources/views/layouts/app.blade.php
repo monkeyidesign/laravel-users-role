f143cc5fc6e9d28b446f884e52a6a92a28f8d861
monkeyidesign <sam@monkeyidesign.com>
2020-04-27 15:33:52 +0700

    Grouping admin route by using namespace and prefix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('users.index') }}">
+                                    <a class="dropdown-item" href="{{ route('admin.users.index') }}">
                                         Users Management
                                     </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function (){
+    Route::resource('/users', 'UsersController', ['except' => ['show', 'create', 'store']]);
+    //php artisan route:list
+});
 
-Route::resource('/admin/users', 'Admin\UsersController', ['except' => ['show', 'create', 'store']]);
-//php artisan route:list
+
